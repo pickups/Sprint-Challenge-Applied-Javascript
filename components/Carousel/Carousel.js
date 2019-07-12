c9d5697db7c1bdda4b55eec7f038db8582063995
astillo <clomes.alex@gmail.com>
2019-07-12 11:41:54 -0500

    got the carousel but it is janky

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,52 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+let imgSrc = ['assets/carousel/mountains.jpeg',
+'assets/carousel/computer.jpeg',
+'assets/carousel/trees.jpeg',
+'assets/carousel/turntable.jpeg']
+
+let count = 0
+
+function createEl(count){
+
+}
+const cara = document.querySelector('.carousel-container')
+cara.appendChild(createAll())
+
+function createAll(){
+  const carDiv = document.createElement('div')
+  const leftDiv = document.createElement('div')
+  const rightDiv = document.createElement('div')
+  const defaultImg = document.createElement('img')
+
+  carDiv.appendChild(leftDiv)
+  carDiv.appendChild(rightDiv)
+  carDiv.appendChild(defaultImg)
+
+  rightDiv.classList.add('right-button')
+  leftDiv.classList.add('left-button')
+  carDiv.classList.add('carousel')
+
+  defaultImg.src = imgSrc[count]
+  defaultImg.style.display = "flex"
+
+  rightDiv.addEventListener('click', ()=>{
+    defaultImg.src = imgSrc[count+1] 
+  })
+
+  leftDiv.addEventListener('click', ()=>{
+    if (count == 0){
+      defaultImg.src = imgSrc[3]
+    }
+    if (count == 1) {
+      defaultImg.src = imgSrc[2]
+    } if (count == 2) {
+      defaultImg.src = imgSrc[0]
+    } if (count == 3) {
+      defaultImg.src = imgSrc[1]
+    }
+  })
+  return carDiv
+}
